6fc3f4b9fd7ad2ccfa39364e5a1234937d0eea8d
Abe <abe149@gmail.com>
2020-04-03 19:25:41 -0400

    a little bit more progress on the engine

--- Configuration_Master_engine.java
+++ Configuration_Master_engine.java
@@ -1,8 +1,11 @@
 import java.io.*;
 
+  
+
 public class Configuration_Master_engine {
 
-  Configuration_Master_engine(BufferedReader input) {
+  Configuration_Master_engine(BufferedReader[] schema_inputs, BufferedReader[] config_inputs) {
+
 
 
 
---
+++ value_types.java
@@ -0,0 +1,10 @@
+public enum value_types {
+              integer,
+  nonnegative_integer,
+     positive_integer,
+
+  nonempty_string,
+           string,
+
+  URL
+}
