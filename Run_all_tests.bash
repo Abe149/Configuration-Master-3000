e6353dd5bc9f6cae276ed3f81dec1d7eb649ba26
Abe <abe149@gmail.com>
2020-04-08 11:11:06 -0400

    "/Run_all_tests.bash": wired in "/test_static-only_expected-to-fail-even-in-nonstrict-mode_tests.bash"

--- Run_all_tests.bash
+++ Run_all_tests.bash
@@ -8,6 +8,9 @@ echo
 ./test_static-only_expected-to-pass-only-in-nonstrict-mode_tests.bash
 exit_code_2=$?
 
-if [ 0 != $exit_code_1 -o 0 != $exit_code_2 ]; then
+./test_static-only_expected-to-fail-even-in-nonstrict-mode_tests.bash
+exit_code_3=$?
+
+if [ 0 != $exit_code_1 -o 0 != $exit_code_2 -o 0 != $exit_code_3 ]; then
   echo -e '\033[31mAt least one test script reported a problem.\033[0m'
 fi
