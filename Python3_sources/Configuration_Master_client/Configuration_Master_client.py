281bfc36c874ccaf542739b874f1345aafa600a1
Abe <abe149@gmail.com>
2020-04-10 14:52:18 -0400

    first working version of "/Python3_sources/Configuration_Master_client/Configuration_Master_client.py", i.e. it has a working "get_config"

--- Python3_sources/Configuration_Master_client/Configuration_Master_client.py
+++ Python3_sources/Configuration_Master_client/Configuration_Master_client.py
@@ -1,15 +1,33 @@
 # _intentionally_ no shebang line here
 
 import os, sys
-from urllib import request
+from urllib         import request
+
+# from urllib.parse import urlencode # unfortunately, "urlencode" does _not_ do what it says in its name IMO, at least not in/on Python 3.2.3
+
+from urllib.request import pathname2url # this seems to do what IMO "urlencode" _should_ do
+
+
+
 assert sys.version_info[0] >= 3
 
+def get_test():
+  pass
+
 def get_config(namespace='*', key=None): # default value for "namespace": only match the key if it`s set for _all_ namespaces within the relevant maturity level; giving "key" a default value b/c otherwise [given that "namespace" has a default value] "key" would have to come _before_ "namespace"
+
+  # if not namespace:  raise ValueError("in Configuration Master 3000 client: empty namespace given")
+  exception_prefix = "in Configuration Master 3000 client: " # DRY
+  if not namespace:  raise ValueError(exception_prefix + "empty namespace given")
+  if not key      :  raise ValueError(exception_prefix + "empty key "  + "given or defaulted")
 
   # same interface as the Bash client
 
   # consciously allowing the following to crash if the env. var.s are not in the env.
-  CONFIG_SERVER_URL     = os.environ["CONFIG_SERVER_URL"]
+  CONFIG_SERVER_URL     = os.environ["CONFIG_SERVER_URL"].rstrip('/') # we need to string trailing slashes from the URL so we can ensure _exactly_ one slash between the end of the "authority" [i.e. either hosthame or IP by itself or hosthame or IP followed immediately by ":<port number>" and the start of "get"
   CONFIG_MATURITY_LEVEL = os.environ["CONFIG_MATURITY_LEVEL"]
-  
 
+  the_request = request.Request(url = CONFIG_SERVER_URL + ("/get:maturity_level=%s,namespace=%s,key=%s" % (pathname2url(CONFIG_MATURITY_LEVEL), pathname2url(namespace), pathname2url(key))))
+
+  with request.urlopen(the_request) as fileLike:
+    return fileLike.read().decode("utf-8")
