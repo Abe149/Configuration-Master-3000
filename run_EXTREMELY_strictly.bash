79d9b12e10f74fd518b7c7760d6dca657b10f1f1
Abe <abe149@gmail.com>
2020-04-10 07:35:04 -0400

    added a/the 9-second countdown code to both "/run_very_strictly.bash" [stamped 2020-04-10 05:40:46.448012997 -0400] & "run_EXTREMELY_strictly.bash" [stamped 2020-04-10 05:40:56.120013245 -0400] ... timestamps of the files included in this commit msg. b/c I got distracted again

--- run_EXTREMELY_strictly.bash
+++ run_EXTREMELY_strictly.bash
@@ -1,9 +1,18 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [ "$0" = sh -o "$0" = /bin/sh -o "$0" = /usr/bin/sh -o "$0" = bash -o "$0" = -bash -o "$0" = /bin/bash -o "$0" = /usr/bin/bash ]; then
   echo 'Please do _not_ source this file!'
   return 1
 fi
+
+
+echo
+for countdown in {9..1}; do
+  echo Are you sure you want to run the server in strict mode?
+  echo Press Control-C [or do something else similar to that] if not...  counting down, $countdown ...
+  echo
+  sleep 1s
+done
 
 ant && java -jar ./Build/jars/Configuration_Master.jar strictness_level=9 verbosity=9 "$@"
 result="$?"
