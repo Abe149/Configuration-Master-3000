32f5a4cf56025769caeffac706958428b0bfaf2c
Abe <abe149@gmail.com>
2020-04-10 15:23:27 -0400

    "/Python3_sources/Configuration_Master_client/test_001_.py": made the test subroutine output _very_ specific about what ML and what namespace it is using for testing [it previously didn`t mention this stuff at all]

--- Python3_sources/Configuration_Master_client/test_001_.py
+++ Python3_sources/Configuration_Master_client/test_001_.py
@@ -8,7 +8,8 @@ os.environ["CONFIG_SERVER_URL"]     = "https://localhost:4430/"
 os.environ["CONFIG_MATURITY_LEVEL"] = '9'
 
 def test(key):
-  print (("%-70s" % ("for key = ''" + key + "'':") + "result = ''" + Configuration_Master_client.get_config(key=key) + "''"))
+  print (("%-70s" % ("in maturity level " + os.environ["CONFIG_MATURITY_LEVEL"] + ", namespace '*' [i.e. only matches config.s that are present in all namespaces at least in this ML], for key = ''" + key + "'':") + "result = ''" + Configuration_Master_client.get_config(key=key) + "''"))
+  print ()
 
 print ()
 test("test good URL")
@@ -18,5 +19,3 @@ test("test good nonnegative_integer")
 test("test good positive_integer")
 test("test key name for a positive integer with redundancy")
 # test("")
-print ()
-
