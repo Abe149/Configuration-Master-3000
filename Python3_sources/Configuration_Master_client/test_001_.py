a2c16e3567e960dd45bfedddf91b16600b515bb1
Abe <abe149@gmail.com>
2020-04-10 14:52:47 -0400

    added "/Python3_sources/Configuration_Master_client/test_001_.py", which already works and has 6 test cases inside it

---
+++ Python3_sources/Configuration_Master_client/test_001_.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+### testing of keys that are expected to be in all namespaces in ML 9 ###
+
+import Configuration_Master_client, os
+
+os.environ["CONFIG_SERVER_URL"]     = "https://localhost:4430/"
+os.environ["CONFIG_MATURITY_LEVEL"] = '9'
+
+def test(key):
+  print (("%-70s" % ("for key = ''" + key + "'':") + "result = ''" + Configuration_Master_client.get_config(key=key) + "''"))
+
+print ()
+test("test good URL")
+test("test good url")
+test("test good nonempty_string")
+test("test good nonnegative_integer")
+test("test good positive_integer")
+test("test key name for a positive integer with redundancy")
+# test("")
+print ()
+
