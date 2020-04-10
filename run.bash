0f50c459ca39595c9543c920772cf4777f8c9025
Abe <abe149@gmail.com>
2020-04-10 19:41:26 -0400

    "/run.bash": used "pushd" & "popd" to make the script able to work correctly; NOTE: I realized too late that I _could_ have left this as a generic-Bourne-shell script and just used "cd" instead of "pushd", since the program is always supposed to be _executed_ [i.e. never _sourced_], and therefor runs in a subprocess of its own, so the "cd" would only affect the SUBprocess, not [also] the _calling_ process

--- run.bash
+++ run.bash
@@ -1,11 +1,13 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 
 if [ "$0" = sh -o "$0" = /bin/sh -o "$0" = /usr/bin/sh -o "$0" = bash -o "$0" = -bash -o "$0" = /bin/bash -o "$0" = /usr/bin/bash ]; then
   echo 'Please do _not_ source this file!'
   return 1
 fi
 
+pushd `dirname "$0"` > /dev/null
 ant && java -jar ./Build/jars/Configuration_Master.jar "$@"
 result="$?"
 /usr/bin/env echo -e "\nResult code: $result"
+popd > /dev/null
 exit $result
