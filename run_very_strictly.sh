7a9a58aed83d0fc20217b020aca119292ff28f35
Abe <abe149@gmail.com>
2020-04-10 03:19:57 -0400

    added "/run_very_strictly.sh", which sets strictness_level=2 [and is otherwise identical to "run_strictly.sh"

---
+++ run_very_strictly.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ "$0" = sh -o "$0" = /bin/sh -o "$0" = /usr/bin/sh -o "$0" = bash -o "$0" = -bash -o "$0" = /bin/bash -o "$0" = /usr/bin/bash ]; then
+  echo 'Please do _not_ source this file!'
+  return 1
+fi
+
+ant && java -jar ./Build/jars/Configuration_Master.jar strictness_level=2 verbosity=9 "$@"
+result="$?"
+/usr/bin/env echo -e "\nResult code: $result"
+exit $result
