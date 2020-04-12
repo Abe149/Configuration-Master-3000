8c3383db44f1531557c97bcaf57be25f6b0db2d4
Abe <abe149@gmail.com>
2020-04-12 07:14:09 -0400

    "/Java_sources/Configuration_Master/utility_class.java": started writing some portability-related helper code [so I`ll be able to do lazy things like call out to Unix "sort" when running on a POSIX-like environment]

--- Java_sources/Configuration_Master/utility_class.java
+++ Java_sources/Configuration_Master/utility_class.java
@@ -2,11 +2,28 @@ package Configuration_Master;
 
 public final class utility_class {
 
+  private utility_class() { } // blocked ctor
+
   public static String stringize_safely(String input) { // TO DO [but low-priority]: pull this out into a separate "library class"
     if (null == input)  return "«null»";
     return "“" + input + "”";
   }
 
-  private utility_class() { }
+  public static boolean does_the_first_param_include_any_of_the_elements_of_the_second_param_as_a_string_prefix
+                        (
+                         String first,
+                         String[] rest
+                        )
+  {
+    return false; // WIP
+  }
+
+  public static boolean are_we_running_on_a_POSIX_environment() { // this is best-effort, due to lack of anything standardized in the Java spec to do this AFAIK
+    return does_the_first_param_include_any_of_the_elements_of_the_second_param_as_a_string_prefix
+             (
+               System.getProperty("os.name"),
+               new String[]{} // WIP
+             );
+  }
 
 }
