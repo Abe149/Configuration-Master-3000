1826e7015cf175adf8467ce05acb15d54b0e4409
Abe <abe149@gmail.com>
2020-04-12 06:30:22 -0400

    "/Run_all_tests.bash": made the ending explicit when everything seems to be OK [previously to this commit, it just succeeded silently]

--- Run_all_tests.bash
+++ Run_all_tests.bash
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+echo
+
 ./test_static-only_expected-to-pass-even-in-strict-mode_tests.bash
 exit_code_1=$?
 
@@ -8,9 +10,15 @@ echo
 ./test_static-only_expected-to-pass-only-in-nonstrict-mode_tests.bash
 exit_code_2=$?
 
+echo
+
 ./test_static-only_expected-to-fail-even-in-nonstrict-mode_tests.bash
 exit_code_3=$?
 
+echo
+
 if [ 0 != $exit_code_1 -o 0 != $exit_code_2 -o 0 != $exit_code_3 ]; then
   echo -e '\033[31mAt least one test script reported a problem.\033[0m'
+else
+  echo 'No problems were detected by inspecting the exit codes of the known-to-this-wrapper test scripts.'
 fi
