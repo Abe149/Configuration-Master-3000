abf6fdfd3d40e13ed579e118a736fadb6e154a35
Abe <abe149@gmail.com>
2020-04-12 01:21:39 -0400

    "/check_non-strictly.sh": moved "strictness_level=0" to the _end_ of the CM3000 invocation, just to make sure that this script will do what it says in its filename even if you try to override that with a CLI arg. like "strictness_level=1", which you should _not_ do

--- check_non-strictly.sh
+++ check_non-strictly.sh
@@ -5,7 +5,7 @@ if [ "$0" = sh -o "$0" = /bin/sh -o "$0" = /usr/bin/sh -o "$0" = bash -o "$0" = 
   return 1
 fi
 
-ant && java -jar ./Build/jars/Configuration_Master.jar check_only strictness_level=0 verbosity=9 "$@"
+ant && java -jar ./Build/jars/Configuration_Master.jar check_only verbosity=9 "$@" strictness_level=0 # "strictness_level=0" is _after_ “"$@"” here on _purpose_, to ensure that this script will operate non-strictly like it says in its filename
 result="$?"
 /usr/bin/env echo -e "\nResult code: $result"
 exit $result
