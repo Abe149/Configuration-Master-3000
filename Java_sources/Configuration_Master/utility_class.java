96aa352395d07e69b165190cdd37e8abcd6fa558
Abe <abe149@gmail.com>
2020-04-12 07:23:04 -0400

    "/Java_sources/Configuration_Master/utility_class.java": more work on the portability-related helper code, again

--- Java_sources/Configuration_Master/utility_class.java
+++ Java_sources/Configuration_Master/utility_class.java
@@ -15,11 +15,22 @@ public final class utility_class {
     return false;
   }
 
+  public static boolean does_the_first_param_start_with_any_of_the_elements_of_the_second_param___ignore_case // ...
+    /* ... */             (String first, String[] rest) {
+    // allowing this to crash if "first" is null
+    for (String foo : rest)  if (first.toLowerCase().startsWith(foo.toLowerCase()))  return true; // slow and stupid
+    return false;
+  }
+
   public static boolean are_we_running_on_a_POSIX_environment() { // this is best-effort, due to lack of anything standardized in the Java spec to do this AFAIK
     return does_the_first_param_start_with_any_of_the_elements_of_the_second_param
              (
                System.getProperty("os.name"),
-               new String[]{} // WIP
+               new String[]{
+                 // strings stolen from <http://commons.apache.org/proper/commons-lang/javadocs/api-release/src-html/org/apache/commons/lang3/SystemUtils.html>; this code written b/c I didn`t want to introduce an external dependency
+                             "AIX", "HP-UX", "Irix", 
+
+                           }
              );
   }
 
