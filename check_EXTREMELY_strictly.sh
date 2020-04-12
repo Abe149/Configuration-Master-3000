be2fd02f6f19712e1917705aa03fd1720be221da
Abe <abe149@gmail.com>
2020-04-12 05:52:01 -0400

    added "/check_very_strictly.sh" & "/check_EXTREMELY_strictly.sh", both cloned from "/check_non-strictly.sh" & edited to do what their respective filenames say they do

---
+++ check_EXTREMELY_strictly.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env sh
+
+if [ "$0" = sh -o "$0" = /bin/sh -o "$0" = /usr/bin/sh -o "$0" = bash -o "$0" = -bash -o "$0" = /bin/bash -o "$0" = /usr/bin/bash ]; then
+  echo 'Please do _not_ source this file!'
+  return 1
+fi
+
+ant && java -jar ./Build/jars/Configuration_Master.jar check_only verbosity=9 "$@" strictness_level=9 # "strictness_level=9" is _after_ “"$@"” here on _purpose_, to ensure that this script will operate like it says in its filename
+result="$?"
+/usr/bin/env echo -e "\nResult code: $result"
+exit $result
