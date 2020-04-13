2c7ad2779bf21df10e836f2a6ca8ab1c56826ec7
Abe <abe149@gmail.com>
2020-04-13 15:54:23 -0400

    started writing new files [in a new subdir.] "/Java_sources/IPv4_client_authorization/IPv4_client_authorization_engine.java" & "/Java_sources/IPv4_client_authorization/strategy_types.java"; they both currently compile OK

---
+++ Java_sources/IPv4_client_authorization/IPv4_client_authorization_engine.java
@@ -0,0 +1,27 @@
+package IPv4_client_authorization;
+
+import java.net.InetAddress;
+import java.util.Set;
+import java.util.HashSet;
+
+// private class engine_state // I realized after typing this: why bother?
+
+public class IPv4_client_authorization_engine {
+
+  private Set<String>      blacklisted_FQDNs = new HashSet<String>();
+  private Set<String>      whitelisted_FQDNs = new HashSet<String>();
+
+  // should I used something "lighter" instead, e.g. "byte[4]"?
+  private Set<InetAddress> blacklisted_IPs   = new HashSet<InetAddress>();
+  private Set<InetAddress> whitelisted_IPs   = new HashSet<InetAddress>();
+
+  private strategy_types the_active_strategy_type = null; // intentionally initializing to an invalid "value"
+
+
+
+
+  boolean is_connection_from_this_address_authorized(InetAddress addr) {
+    return false; // WIP WIP WIP //
+  }
+
+} // end of class "IPv4_client_authorization_engine"
---
+++ Java_sources/IPv4_client_authorization/strategy_types.java
@@ -0,0 +1,9 @@
+package IPv4_client_authorization;
+
+public enum strategy_types {
+
+  any_client_that_meets_all_active_requirements,
+  blacklisting,
+  whitelisting
+
+}
